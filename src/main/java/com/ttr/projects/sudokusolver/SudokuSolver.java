161662b3d84fcddc78b04170b2ea7cb52dd8ac43
ttr <thomas.troussellier@gmail.com>
2018-03-17 15:50:50 +0100

    add pom.xml, main class and update gitignore

---
+++ src/main/java/com/ttr/projects/sudokusolver/SudokuSolver.java
@@ -0,0 +1,7 @@
+package com.ttr.projects.sudokusolver;
+
+public class SudokuSolver {
+    public static void main(String[] args) {
+        System.out.println("Start of our glorious project");
+    }
+}
